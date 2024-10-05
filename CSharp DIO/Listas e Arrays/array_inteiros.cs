2e82baf519e3d6f9f230d6eab7e4ae00126ed85d
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2024-10-05 05:57:42 -0400

    a

---
+++ CSharp DIO/Listas e Arrays/array_inteiros.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_DIO.Listas_e_Arrays
+{
+    internal class array_inteiros
+    {
+        public static void main()
+        {
+            Console.WriteLine("Estou Executando....");
+        }
+    }
+}
---
+++ CSharp DIO/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CSharp_DIO.Listas_e_Arrays;
+
+Console.WriteLine("Hello, World!");
+
+array_inteiros.main();
